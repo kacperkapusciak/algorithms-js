e387be574871a7291dbf7a5645d8df42371cd1e6
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-03-31 23:56:54 +0200

    Binary search

---
+++ binarySearch.js
@@ -0,0 +1,28 @@
+//in binary search provided array have to be sorted 
+//because none of it would make sense
+
+//time complexity
+//worst case O(log_2 n) 
+//best case O(1)
+
+const binarySearch = (arr, elem) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) 
+      end = middle - 1;
+    else 
+      start = middle + 1;
+    
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === elem ? middle : -1;
+}
+
+const result = binarySearch([2,3,4,5,6,7,8,9,10], 8);
+
+console.log(result);
+

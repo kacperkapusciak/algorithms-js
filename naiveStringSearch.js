fc14d3cc0791a7d75d33f204c6d841b9a7a9ac4e
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-04-01 00:13:46 +0200

    Naive string search

---
+++ naiveStringSearch.js
@@ -0,0 +1,14 @@
+const naiveStringSearch = (string, pattern) => {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      if (pattern[j] !== string[i + j]) break;
+      if (j === pattern.length - 1) count++;
+    }
+  }
+  return count;
+};
+
+const result = naiveStringSearch("qwertyuiopasdfghjklasdf", "asdf");
+
+console.log(result);

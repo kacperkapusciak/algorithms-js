3d4ba3a19b7ec8a6acbd32a9f05bf9ef8a9dc818
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-04-03 22:32:07 +0200

    Singly linked list (#1)
    * Added push
    
    * Added print
    
    * Added pop
    
    * Added shift
    
    * Added unshift
    
    * Added get
    
    * Added set
    
    * Added insert
    
    * Added remove
    
    * Added reverse
    
    * Improvement

---
+++ singlyLinkedList.js
@@ -0,0 +1,147 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return true;
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = this.head;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length == 0) this.head = this.tail = null;
+
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    const temp = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    if (this.length == 0) this.tail = null;
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return true;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let current = this.head;
+    for (let counter = 0; index !== counter; counter++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  set(index, value) {
+    const found = this.get(index);
+    if (found) {
+      found.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(value);
+    if (index === 0) return this.unshift(value);
+
+    const newNode = new Node(value);
+    const prev = this.get(index - 1);
+    // let temp = prev.next;
+    // prev.next = newNode;
+    // newNode.next = temp;
+    [prev.next, newNode.next] = [newNode, prev.next]; //tricky but shorter and it works!
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    const prev = this.get(index - 1);
+    const removed = prev.next;
+    prev.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    [this.head, this.tail] = [this.tail, this.head];
+    let prev = null;
+    let node = this.tail;
+    let next;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next; 
+    }
+  }
+}
+
+let list = new SinglyLinkedList();
+
+list.push("Hi");
+list.push("it's");
+list.push("me!");
+list.pop();
+list.shift();
+list.unshift("me!!");
+list.print();
+list.insert(1, "Hello");
+console.log("////////////////")
+list.reverse();
+list.print();

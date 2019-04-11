46f0cafee12b3e6ed817cd45c4829b6e857985a1
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-04-11 23:21:23 +0200

    Added push and print

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -12,4 +12,33 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+
+list.print();
+
+

91c34752b6ebf9470611fb4a8ad3ba341d9e5c82
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-04-14 16:59:30 +0200

    Stack and queue (#3)
    * Added stack
    
    * Added queue

---
+++ queue.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    const temp = this.first;
+    if (this.first === this.last) this.last = null;
+
+    this.first = this.first.next;
+    this.size--;
+
+    return temp.value;
+  }
+
+  print() {
+    let current = this.first;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(123);
+queue.enqueue(234);
+queue.enqueue(345);
+queue.print();
+console.log("---");
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
---
+++ stack.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    const temp = this.first; 
+    this.first = this.first.next;
+    this.size--;
+
+    if (this.size == 0) this.last = null;
+    return temp.value;
+  }
+
+  print() {
+    let current = this.first;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+const stack = new Stack();
+stack.push(123);
+stack.push(234);
+stack.push(345);
+stack.print();
+console.log("---")
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());

0e420ba05cf81d0f8a849c95ad93184edc83553c
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-04-19 21:44:32 +0200

    Added more tests to singly linked list

--- __tests__/singlyLinkedList.test.js
+++ __tests__/singlyLinkedList.test.js
@@ -24,6 +24,7 @@ describe("Singly linked list", () => {
     list.push(1);
     expect(list.head.value).toBe(1);
     expect(list.tail.value).toBe(1);
+
     for (let i of [2, 3, 4, 5, 6, 7, 8, 9, 10]) 
       list.push(i);
 
@@ -33,8 +34,7 @@ describe("Singly linked list", () => {
   });
 
   test("pop method should remove item from the end", () => {
-    for (let i of [1, 2, 3]) 
-      list.push(i);
+    [1, 2, 3].forEach((i) => list.push(i));
 
     expect(list.length).toBe(3);
 
@@ -49,8 +49,7 @@ describe("Singly linked list", () => {
   });
 
   test("shift method should remove item from the beginning", () => {
-    for (let i of [1, 2, 3]) 
-      list.push(i);
+    [1, 2, 3].forEach((i) => list.push(i));
 
     expect(list.length).toBe(3);
 
@@ -74,16 +73,67 @@ describe("Singly linked list", () => {
     expect(list.tail.value).toBe(1);
 
     for (let i of [2, 3, 4, 5, 6, 7, 8, 9, 10]) 
-    list.unshift(i);
+      list.unshift(i);
 
-  expect(list.head.value).toBe(10);
-  expect(list.tail.value).toBe(1);
-  expect(list.length).toBe(10);
+    expect(list.head.value).toBe(10);
+    expect(list.tail.value).toBe(1);
+    expect(list.length).toBe(10);
   });
 
-  test.todo("test get method");
-  test.todo("test set method");
-  test.todo("test insert method");
-  test.todo("test remove method");
-  test.todo("test reverse method");
+  test("get method should return item of the given index", () => {
+    [1, 2, 3].forEach((i) => list.push(i));
+
+    expect(list.get(0).value).toBe(1);
+    expect(list.get(1).value).toBe(2);
+    expect(list.get(2).value).toBe(3);
+
+    expect(list.get(-1)).toBeNull();
+    expect(list.get(100)).toBeNull();
+  });
+
+  test("set method should set item on the given index", () => {
+    [1, 2, 3].forEach((i) => list.push(i));
+
+    list.set(1, "test");
+    expect(list.get(1).value).toBe("test");
+    expect(list.length).toBe(3);
+  });
+
+  test("insert method should insert item between nodes", () => {
+    list.insert(0, 0);
+    expect(list.get(0).value).toBe(0);
+
+    [1, 2, 3].forEach((i) => list.push(i));
+
+    list.insert(1, 11);
+    expect(list.get(1).value).toBe(11);
+    expect(list.length).toBe(5);
+
+    expect(list.insert(-1)).toBeFalsy();
+    expect(list.insert(1000)).toBeFalsy();
+  });
+
+  test("remove method should remove and return item of given index", () => {
+    [0, 1, 2, 3, 4, 5].forEach((i) => list.push(i));
+
+    expect(list.remove(3).value).toBe(3);
+    expect(list.length).toBe(5);
+    expect(list.remove(-1)).toBeUndefined();
+    expect(list.remove(6)).toBeUndefined();
+
+    expect(list.remove(0).value).toBe(0);
+    expect(list.remove(3).value).toBe(5);
+  });
+
+  test("reverse method should reverse the list", () => {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    arr.forEach((el) => list.push(el));
+
+    list.reverse();
+    arr.reverse(); //build in array reverse method
+
+    arr.forEach((item, index) => {
+      expect(list.get(index).value).toBe(item);
+    });
+  });
 });

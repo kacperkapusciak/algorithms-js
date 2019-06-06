a8b45c5d6d5ff8789b35edf2d73ca112deb7d4df
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-06-06 20:15:27 +0200

    Binary search tree (#5)
    * Added --watchAll option to jest
    
    * Implemented and tested insert method
    
    * Implemented and tested find method

---
+++ src/binarySearchTree.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(value) {
+    if(this.root === null) return false;
+    let current = this.root,
+        found = false;
+    while ( current && !found ) {
+      if ( value < current.value )
+        current = current.left;
+      else if ( value > current.value)
+        current = current.right;
+      else 
+        found = true;
+    } 
+    return found ? current : false;
+  }
+}
+
+module.exports = {
+  Node,
+  BinarySearchTree
+};
